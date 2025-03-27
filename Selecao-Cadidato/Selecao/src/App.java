d218a4942c975dc806cc714a754ed5ad26032c65
Altovisk09 <ericaltao13@gmail.com>
2025-03-27 16:59:08 -0300

    formatação para melhor visualização no console

--- Selecao-Cadidato/Selecao/src/App.java
+++ Selecao-Cadidato/Selecao/src/App.java
@@ -1,11 +1,15 @@
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
+
+        System.out.println("==================== PROCESSO SELETIVO ====================\n");
         ligarParaCandidatosSelecionados(selecaoCadidatos());
+        System.out.println("\n==================== PROCESSO FINALIZADO ====================\n");
     };
 
     public static void analisarCandidatos(double salarioPretendido) {
@@ -21,8 +25,12 @@ public class App {
     public static List<String> selecaoCadidatos() {
         String[] candidatos = { "Matheus", "Julia", "Kaylane", "Jamile", "Pedro", "Thales" };
 
+        System.out.println("\nParticipantes do processo:\n" + Arrays.toString(candidatos));
+
         List<String> candidatosSelecionados = new ArrayList<>();
         double salarioProposto = 2000.0;
+
+        System.out.println("\nFILTRANDO CANDIDATOS...\n");
 
         for (int i = 0; i < candidatos.length; i++) {
             String candidato = candidatos[i];
@@ -31,19 +39,21 @@ public class App {
 
             if (salarioPretendido <= salarioProposto && candidatosSelecionados.size() <= 5) {
                 candidatosSelecionados.add(candidato);
-                System.out.println("O candidato " + candidato + " foi selecionado para a vaga\n");
             } else {
-                System.out.println("salario pretendido (" + salarioFormatado + ") fora do escopo da empresa.\n");
+                System.out.println("Candidato " + candidato + " RECUSADO (R$ " + salarioFormatado
+                        + ") salario fora do escopo.");
             }
         }
         ;
-
         System.out.println("\nLista de candidatos selecionados:\n" + candidatosSelecionados);
 
         return candidatosSelecionados;
     };
 
     static void ligarParaCandidatosSelecionados(List<String> lista) {
+        System.out.println("\n================ TENTANDO CONTATO COM APROVADOS =================");
+        List<String> finalistas = new ArrayList<>();
+
         for (String candidato : lista) {
             int tentativasLigacao = 0;
             boolean atendeu = false;
@@ -55,13 +65,16 @@ public class App {
                 if (atendeu) {
                     atendeu = true;
                     System.out.println(
-                            "\nCandidato " + candidato + " atendeu na " + tentativasLigacao + "tentativa, aprovado");
-                } 
+                            "\nCandidato " + candidato + " atendeu na " + tentativasLigacao + " tentativa, aprovado");
+                    finalistas.add(candidato);
+                }
             }
             if (!atendeu) {
                 System.out.println("\nCandidato " + candidato + " não atendeu após 3 tentativas, desqualificado.");
             }
         }
+        System.out.println("\n==================== FINALISTAS ====================\n");
+        System.out.println(finalistas);
 
     };
 
